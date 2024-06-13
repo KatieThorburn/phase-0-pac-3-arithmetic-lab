3d0c3bef4c19a01ae55393cebc5f17cadc0e8b6c
Katie <thorburnkatie@gmail.com>
2024-06-13 13:49:39 +1000

    almost completed, change computers

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+}
+function multiply(a, b){
+    return a * b
+}
+function divide(a, b){
+    return a / b
+}
+function increment(a){
+    return a + 1
+}
+function decrement(a){
+    return a - 1
+}
+function makeInt(n){
+    
+}
+console.log(add());
+console.log(subtract());
+console.log(multiply());
+console.log(divide());
+console.log(increment());
+console.log(decrement());
